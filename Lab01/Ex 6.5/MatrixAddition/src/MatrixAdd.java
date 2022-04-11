9f2b13889652080b6d6fc346c5faa5454910d3a6
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-04-11 16:21:45 +0700

    Change source code of MatrixAdd - Lab01

--- Lab01/Ex 6.5/MatrixAddition/src/MatrixAdd.java
+++ Lab01/Ex 6.5/MatrixAddition/src/MatrixAdd.java
@@ -3,6 +3,7 @@ public class MatrixAdd {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter the size of matrices: ");
 		int m, n;
 		m = sc.nextInt();
 		n = sc.nextInt();
@@ -11,10 +12,21 @@ public class MatrixAdd {
 		int[][] matrix2 = new int[m][n];
 		int[][] sumMatrix = new int[m][n];
 		
-		for(int i=0; i<m; i++) {
+		System.out.println("Enter the 1st matrix: ");
+		for (int i=0; i<m; i++) {
 			for (int j=0; j<n; j++) {
 				matrix1[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Enter the 2nd matrix: ");
+		for (int i=0; i<m; i++) {
+			for (int j=0; j<n; j++) {
 				matrix2[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("The sum matrix is: ");
+		for (int i=0; i<m; i++) {
+			for (int j=0; j<n; j++) {
 				sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
 				System.out.print(sumMatrix[i][j] + "\t");
 			}
