0404a5bcc6b91b03fee604b636ae2cf8fb0f4e3e
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-05-07 14:17:17 +0700

    Create class of cart and test it by adding dvd

--- AimsProject/Lab03/AimsProject/src/Aims.java
+++ AimsProject/Lab03/AimsProject/src/Aims.java
@@ -1,9 +1,28 @@
+import features.Cart;
+import features.DigitalVideoDisc;
 
 public class Aims {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//create a new cart
+		Cart anOrder = new Cart();
+				
+		//create new dvd objects and add them to the cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+						"Animation", "Roger Allers", 87, 19.95f);
+		anOrder.addDigitalVideoDisc(dvd1);
+				
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
+						"Science Fiction", "George Lucas", 87, 24.95f);
+		anOrder.addDigitalVideoDisc(dvd2);
+				
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", 
+						"Animation", 18.99f);
+		anOrder.addDigitalVideoDisc(dvd3);
+				
+		//print total cost of the items in the cart
+		System.out.println("The total cost is: ");
+		System.out.println(anOrder.totalCost());
 	}
 
 }
