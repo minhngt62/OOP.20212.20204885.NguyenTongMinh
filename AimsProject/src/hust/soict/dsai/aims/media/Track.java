59ad051207adb56b89051c4ec427d9426706a783
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-06-06 00:40:42 +0700

    Create new media: Track

---
+++ AimsProject/src/hust/soict/dsai/aims/media/Track.java
@@ -0,0 +1,33 @@
+package hust.soict.dsai.aims.media;
+
+public class Track implements Playable {
+	private String title;
+	private int length;
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+
+	public Track(String title) {
+		this.title = title;
+	}
+	public Track(String title, int length) {
+		this.title = title;
+		this.length = length;
+	}
+	
+	public boolean equals(Track track) {
+		return this.title.equals(track.title);
+	}
+	
+	public void play() {
+		System.out.println("Playing track: " + this.getTitle());
+		System.out.println("Track length: " + this.getLength());
+	}
+
+}
