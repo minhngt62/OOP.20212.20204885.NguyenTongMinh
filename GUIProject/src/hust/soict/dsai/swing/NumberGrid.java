353dce2b7b37c77d7d83f0c4cf4fce19e1cbe97c
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-06-17 23:27:10 +0700

    Practice layout management with NumberGrid

---
+++ GUIProject/src/hust/soict/dsai/swing/NumberGrid.java
@@ -0,0 +1,77 @@
+package hust.soict.dsai.swing;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class NumberGrid extends JFrame {
+	private JButton[] btnNumbers = new JButton[10];
+	private JButton btnDelete, btnReset;
+	private JTextField tfDisplay;
+	
+	public NumberGrid() {
+		tfDisplay = new JTextField();
+		tfDisplay.setComponentOrientation(
+				ComponentOrientation.RIGHT_TO_LEFT);
+		
+		JPanel panelButtons = new JPanel(new GridLayout(4, 3));
+		addButtons(panelButtons);
+		
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+		cp.add(tfDisplay, BorderLayout.NORTH);
+		cp.add(panelButtons, BorderLayout.CENTER);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Number Grid");
+		setSize(200, 200);
+		setVisible(true);
+	}
+	
+	void addButtons(JPanel panelButtons) {
+		ButtonListener btnListener = new ButtonListener();
+		for(int i = 1; i <= 9; i++) {
+			btnNumbers[i] = new JButton(""+i);
+			panelButtons.add(btnNumbers[i]);
+			btnNumbers[i].addActionListener(btnListener);
+		}
+		
+		btnDelete = new JButton("DEL");
+		panelButtons.add(btnDelete);
+		btnDelete.addActionListener(btnListener);
+		
+		btnNumbers[0] = new JButton("0");
+		panelButtons.add(btnNumbers[0]);
+		btnNumbers[0].addActionListener(btnListener);
+		
+		btnReset = new JButton("C");
+		panelButtons.add(btnReset);
+		btnReset.addActionListener(btnListener);
+	}
+	
+	private class ButtonListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String button = e.getActionCommand();
+			if(button.charAt(0) >= '0' && button.charAt(0) <= '9') {
+				tfDisplay.setText(tfDisplay.getText() + button);
+			}
+			else if(button.equals("DEL")) {
+				tfDisplay.setText(delLastCharacter(tfDisplay.getText()));
+			}
+			else {
+				tfDisplay.setText("");
+			}
+		}
+	}
+	
+	public static String delLastCharacter(String str) {
+		   if ((str != null) && (str.length() > 0)) {
+		      return str.substring(0, str.length() - 1);
+		   }
+		   return null;
+	}
+	
+	public static void main(String[] args) {
+		new NumberGrid();
+	}
+}
