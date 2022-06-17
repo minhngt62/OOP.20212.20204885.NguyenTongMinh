2eca2029d4057dcebff10a5cd0fb659c7425fbc5
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-06-18 00:39:31 +0700

    Create login screen

---
+++ AimsProject/src/hust/soict/dsai/aims/screen/StoreManagerScreen.java
@@ -0,0 +1,15 @@
+package hust.soict.dsai.aims.screen;
+
+import hust.soict.dsai.aims.store.Store;
+
+import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
+
+public class StoreManagerScreen {
+
+	public StoreManagerScreen(Store store) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
